b66cfd1ad3aa0f8ac5852273ebc6f8bf25ec259b
Ravi Bhure <ravibhure@gmail.com>
2012-01-24 22:38:42 +0530

    updated slave_sync-async recipes

--- cookbooks/db_postgres/recipes/do_show_slave_sync_mode.rb
+++ cookbooks/db_postgres/recipes/do_show_slave_sync_mode.rb
@@ -8,7 +8,7 @@
 rs_utils_marker :begin
 
 # Run only on master server
-if (rightscale[:instance_uuid] = "#{node[:db][:current_master_uuid]}")
+if node[:rightscale][:instance_uuid] == "#{node[:db][:current_master_uuid]}"
 #
 # Show sync mode status
 #
