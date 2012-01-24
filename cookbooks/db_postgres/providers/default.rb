6f67d28d14fdf3e5835a9f7979b90ec71400ace9
Ravi Bhure <ravibhure@gmail.com>
2012-01-25 00:44:14 +0530

    updated do_nothing after reboot

--- cookbooks/db_postgres/providers/default.rb
+++ cookbooks/db_postgres/providers/default.rb
@@ -168,10 +168,6 @@ end
 
 action :install_server do
 
-  
-  touchfile = ::File.expand_path "~/.postgresql_installed"
-  raise "Skipping the action" if File.exists?(touchfile)
-
   # PostgreSQL server depends on PostgreSQL client
   action_install_client
 
@@ -199,6 +195,7 @@ action :install_server do
   end
 
   # Initialize PostgreSQL server and create system tables
+  touchfile = ::File.expand_path "~/.postgresql_installed"
   execute "/etc/init.d/postgresql-#{node[:db_postgres][:version]} initdb ; touch #{touchfile}" do
     creates touchfile
   end
@@ -223,13 +220,15 @@ action :install_server do
 
   # Setup postgresql.conf
   # template_source = "postgresql.conf.erb"
-
+   
+  touchfile = ::File.expand_path "~/.postgresql_config.done"
   template value_for_platform([ "centos", "redhat", "suse" ] => {"default" => "#{node[:db_postgres][:confdir]}/postgresql.conf"}, "default" => "#{node[:db_postgres][:confdir]}/postgresql.conf") do
     source "postgresql.conf.erb"
     owner "postgres"
     group "postgres"
     mode "0644"
     cookbook 'db_postgres'
+    not_if {File.exists?("touchfile")}
   end
 
   # Setup pg_hba.conf
@@ -240,8 +239,14 @@ action :install_server do
     group "postgres"
     mode "0644"
     cookbook 'db_postgres'
+    not_if {File.exists?("touchfile")}
   end
 
+  # Ensure we have done the postgresql config setup sucessfully, this will take care to do not overwritten these after reboot. 
+  execute "touch #{touchfile}" do
+    creates touchfile
+  end
+  
   # == Setup PostgreSQL user limits
   #
   # Set the postgres and root users max open files to a really large number.
