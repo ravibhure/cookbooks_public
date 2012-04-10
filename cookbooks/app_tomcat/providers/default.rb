76c8fd964aa3c2e7332e06e235647923db9869e6
Ravi Bhure <ravibhure@gmail.com>
2012-04-10 21:45:16 +0530

    fixed variable issue in tomcat setup_monitoring action

--- cookbooks/app_tomcat/providers/default.rb
+++ cookbooks/app_tomcat/providers/default.rb
@@ -425,8 +425,6 @@ action :setup_monitoring do
   bash "Add collectd to setenv" do
     flags "-ex"
     code <<-EOH
-     # set_opt="\$""CATALINA_OPTS"
-      #cat <<EOF>>/usr/share/apache-tomcat-#{node[:tomcat][:version]}/bin/setenv.sh
     echo  'CATALINA_OPTS="\$CATALINA_OPTS -Djcd.host=#{node[:rightscale][:instance_uuid]} -Djcd.instance=tomcat7 -Djcd.dest=udp://#{node[:rightscale][:servers][:sketchy][:hostname]}:3011 -Djcd.tmpl=javalang,tomcat -javaagent:/usr/share/apache-tomcat-#{node[:tomcat][:version]}/lib/collectd.jar"' >> /usr/share/apache-tomcat-#{node[:tomcat][:version]}/bin/setenv.sh
     EOH
   end
